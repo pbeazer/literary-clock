071fc04f34420b702f1d0f120e17dec89e02fbe1
Adam Brakhane <adam.brakhane@gmail.com>
2022-07-10 15:33:45 -0600

    only show the current time if the image is out of date

--- literary_clock.py
+++ literary_clock.py
@@ -81,15 +81,14 @@ def main():
 	now = datetime.now()
 
 	quotes,firstTry=getTimeQuotes(now,0)
-	
+
 	# Write the quote to screen
 	quote = quotes[randrange(0, len(quotes))]
 	quoteImage = Image.open(quote).convert('1')
 	image.paste(quoteImage, (0, 80))
 
 	# If not our first try, also write the current time
-	# if !firstTry:
-	if True:
+	if !firstTry:
 		now_time = now.strftime('%I:%M %p')
 		draw_time = ImageDraw.Draw(image)
 		time_font = ImageFont.truetype('Literata72pt-Regular.ttf', 30)
